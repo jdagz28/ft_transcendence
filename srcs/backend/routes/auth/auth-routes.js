6bde4adff5edead350e59f0000c65198b68106a1
jdagz28 <joshdagoy28@gmail.com>
2025-04-04 00:02:15 +0200

    to Ubuntu

---
+++ srcs/backend/routes/auth/auth-routes.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+
+const generateHash = require('./generate-hash')
+
+module.exports.prefixOverride = ''
+module.exports = fp(
+  async function applicationAuth (fastify, opts) {
+    fastify.post('/signup', {
+      schema: {
+        body: fastify.getSchema('schema:auth:signup')
+      },
+      handler: 
+    })
+  }
+)
