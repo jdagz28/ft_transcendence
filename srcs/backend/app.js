46c3d7940a7e946feae3399e7878a6737a64615c
jdagz28 <joshdagoy28@gmail.com>
2025-04-03 22:42:35 +0200

    reset fastify init

--- srcs/backend/app.js
+++ srcs/backend/app.js
@@ -1,53 +1,39 @@
-const db = require('./database')
-const fastify = require('fastify')
-//! Define other server options
-const serverOptions = {
-    logger: true
+'use strict'
+
+const path = require('node:path')
+const AutoLoad = require('@fastify/autoload')
+
+// Pass --options via CLI arguments in command to enable these options.
+const options = {}
+
+module.exports = async function (fastify, opts) {
+  // Place here your custom code!
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'schemas'),
+    indexPattern: /^loader.js$/i
+  })
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    dirNameRoutePrefix: false,
+    options: Object.assign({}, opts)
+  })
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    indexPattern: /.*routes(\.js|\.cjs)$/i,
+    ignorePattern: /.*\.js/,
+    autoHooksPattern: /.*hooks(\.js|\.cjs)$/i,
+    autoHooks: true,
+    cascadeHooks: true,
+    options: Object.assign({}, opts)
+  })
 }
 
-const app = fastify(serverOptions)
-
-app.route({
-    url: '/',
-    method: 'GET',
-    onRequest: async (request, reply) => {
-        if (!isAuthenticated(request)) {
-            return reply.redirect('/login');
-        }
-    },
-    handler: indexHandler
-})
-
-app.route({
-    url: '/login',
-    method: 'GET',
-    handler: loginHandler
-})
-
-function isAuthenticated (request) {
-    /**
-     * ! Implement authenticated user check
-     * * session-cookie, JWT, etc.
-     */
-    return false;
-}
-
-async function indexHandler (request, reply) {
-    reply.send({ helloFrom: this.server.address() })
-}
-
-async function loginHandler (request, reply) {
-    reply.send({ helloFrom: '/login' })
-}
-
-app.listen({
-    port: 4242,
-    host: '0.0.0.0'
-})
-    .then((address) => {
-         console.log(`Server listening on ${address}`)
-    })
-    .catch(err => {
-        console.error(err);
-        process.exit(1);
-    });
+module.exports.options = options
