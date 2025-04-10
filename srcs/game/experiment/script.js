e235988a7041762b4388c623da5b22bd21f30d00
jdagz28 <joshdagoy28@gmail.com>
2025-04-10 11:54:29 +0200

    movable paddles

--- srcs/game/experiment/script.js
+++ srcs/game/experiment/script.js
@@ -1,20 +1,67 @@
 var canvas = document.querySelector("canvas")
-
 var ctx = canvas.getContext('2d')
+
+
+var paddleHeight = 200
+var RequestFrame = false
+var WKeyState = false
+var SKeyState = false
+var ArrowUpKeyState = false
+var ArrowDownKeyState = false
+var dirX = false
+var dirY = false
+
+document.addEventListener('keydown', (e) => {
+  if (e.key == "w")
+    WKeyState = true
+  if (e.key == "s")
+    SKeyState = true
+  if (e.key == "ArrowUp")
+    ArrowUpKeyState = true
+  if (e.key == "ArrowDown")
+    ArrowDownKeyState = true
+  if (e.key == "Enter") {
+    if (!RequestFrame) {
+      var ball = new Obj(DocWidth / 2, DocHeight / 2, 15)
+      ball.drawBall()
+      RequestFrame = true
+      MoveBallLoop(ball)
+    }
+  }
+})
+
+document.addEventListener('keyup', (e) => {
+  if (e.key == "w")
+    WKeyState = false
+  if (e.key == "s")
+    SKeyState = false
+  if (e.key == "ArrowUp")
+    ArrowUpKeyState = false
+  if (e.key == "ArrowDown")
+    ArrowDownKeyState = false
+})
 
 var DocHeight,DocWidth
 function canvasSetup() {
-    DocHeight = window.innerHeight * 0.75
-    DocWidth = window.innerWidth
-    canvas.height = DocHeight
-    canvas.width = DocWidth
-    canvas.style.backgroundColor = "black"
+  DocHeight = window.innerHeight * 0.75
+  DocWidth = window.innerWidth
+  canvas.height = DocHeight
+  canvas.width = DocWidth
+  canvas.style.backgroundColor = "black"
+  drawCenterLine()
+
+  Pad1YPos = DocHeight / 2
+  Pad2YPos = DocHeight / 2
+  DrawPads(Pad1YPos, Pad2YPos)
+
+  var ball = new Obj(DocWidth / 2, DocHeight / 2, 15)
+  ball.drawBall()
 }
 
 
 function DrawPads(Pad1YPos, Pad2YPos) {
-  var Pad1 = new Obj(50, Pad1YPos, 25, 200)
-  var Pad2 = new Obj(DocWidth - 50 - 25, Pad2YPos, 25, 200)
+  var Pad1 = new Obj(50, Pad1YPos, 25, paddleHeight)
+  var Pad2 = new Obj(DocWidth - 50 - 25, Pad2YPos, 25, paddleHeight)
   Pad1.drawPad()
   Pad2.drawPad()
 }
@@ -23,23 +70,41 @@ class Obj {
   constructor(x, y, width, height) {
     this.color = "white"
     this.x = x
-    this.y = y - (height / 2)
+    this.y = height !== undefined ? y - (height / 2) : y;
     this.width = width
     this.height = height
     this.speed = 8
+  }
 
+  drawBall() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.width, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+    ctx.closePath()
   }
 
   drawPad() {
     ctx.fillStyle = this.color
     ctx.fillRect(this.x, this.y, this.width, this.height)  
   }
-}
 
+  moveBall() {
+    ctx.clearRect(0, 0, DocWidth + 100, DocHeight)
+    DrawPads(Pad1YPos, Pad2YPos)
+    if (dirY) this.y += this.speed
+    if (dirX) this.x += this.speed
+    if (!dirY) this.y -= this.speed
+    if (!dirX) this.x -= this.speed
+    drawCenterLine
+    this.drawBall()
+  }
+  
+}
 
 function drawCenterLine() {
   ctx.strokeStyle = "white";
-  ctx.lineWidth = 5;
+  ctx.lineWidth = 3;
   ctx.setLineDash([10, 10]);
   
   ctx.beginPath();
@@ -50,13 +115,26 @@ function drawCenterLine() {
   ctx.setLineDash([]);
 }
 
+function MoveBallLoop(ball) {
+  if (WKeyState && Pad1YPos > paddleHeight / 2) Pad1YPos -= 10
+  if (SKeyState && Pad1YPos < DocHeight - paddleHeight / 2) Pad1YPos += 10
+  if (ArrowUpKeyState && Pad2YPos > paddleHeight / 2) Pad2YPos -= 10
+  if (ArrowDownKeyState && Pad2YPos < DocHeight - paddleHeight / 2) Pad2YPos += 10
+
+  ctx.clearRect(0, 0, DocWidth + 100, DocHeight)
+
+  drawCenterLine()
+  DrawPads(Pad1YPos, Pad2YPos)
+
+  ball.moveBall()
+  if (RequestFrame) requestAnimationFrame(() => { MoveBallLoop(ball) })
+}
+
 
 
 
 canvasSetup()
 
-Pad1YPos = DocHeight / 2
-Pad2YPos = DocHeight / 2
-DrawPads(Pad1YPos, Pad2YPos)
 
-drawCenterLine()
+
+
