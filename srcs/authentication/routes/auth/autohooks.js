c04f1b50e8519ad207552ea75c5441a495ec1a95
jdagz28 <joshdagoy28@gmail.com>
2025-04-12 00:18:36 +0200

    Database Container - /users/:username endpoint; tested get query and connection from authentication container

--- srcs/authentication/routes/auth/autohooks.js
+++ srcs/authentication/routes/auth/autohooks.js
@@ -2,6 +2,7 @@
 
 const fp = require('fastify-plugin')
 const schemas = require('./schemas/loader')
+const axios = require('axios')
 
 module.exports = fp(async function userAutoHooks (fastify, opts) {
   fastify.register(schemas)
@@ -9,42 +10,52 @@ module.exports = fp(async function userAutoHooks (fastify, opts) {
   fastify.decorate('usersDataSource', {
     async readUser(username) {
       try {
-        const query = fastify.db.prepare('SELECT * FROM users WHERE username = ?')
-        const row = query.get(username)
-        fastify.log.debug(`readUser: ${username} -> %`, row)
-        return row
+        const response = await axios.get(`http://database:1919/users/${username}`);
+        fastify.log.info(`readUser: ${username} ->`, response.data)
+        return response.data
       } catch (err) {
         fastify.log.error(`readUser error: ${err.message}`)
         throw new Error('User lookup failed')
       }
-    },
-
-    async readUserByEmail(email) {
-      try {
-        const query = fastify.db.prepare('SELECT * FROM users WHERE email = ?')
-        const row = query.get(email)
-        fastify.log.debug(`readUserByEmail: ${email} -> %j`, row)
-        return row
-      } catch (err) {
-        fastify.log.error(`readUserByEmail error: ${err.message}`)
-        throw new Error('Email lookup failed')
-      }
-    },
-
-    async createUser(user) {
-      try {
-        const { username, password, salt, email } = user
-        const query = fastify.db.prepare(`INSERT INTO users (username, password, salt, email) VALUES (?, ?, ?, ?)`)
-        const result = query.run(username, password, salt, email)
-        fastify.log.debug(`createUser: ${username} -> ID ${result.lastInsertRowid}`)
-        return result.lastInsertRowid
-      } catch (err) {
-        fastify.log.error(`createUser error: ${err.message}`)
-        throw new Error('User creation failed')
-      }
     }
+
+    // async readUser(username) {
+    //   try {
+    //     const query = fastify.db.prepare('SELECT * FROM users WHERE username = ?')
+    //     const row = query.get(username)
+    //     fastify.log.debug(`readUser: ${username} -> %`, row)
+    //     return row
+    //   } catch (err) {
+    //     fastify.log.error(`readUser error: ${err.message}`)
+    //     throw new Error('User lookup failed')
+    //   }
+    // },
+
+    // async readUserByEmail(email) {
+    //   try {
+    //     const query = fastify.db.prepare('SELECT * FROM users WHERE email = ?')
+    //     const row = query.get(email)
+    //     fastify.log.debug(`readUserByEmail: ${email} -> %j`, row)
+    //     return row
+    //   } catch (err) {
+    //     fastify.log.error(`readUserByEmail error: ${err.message}`)
+    //     throw new Error('Email lookup failed')
+    //   }
+    // },
+
+    // async createUser(user) {
+    //   try {
+    //     const { username, password, salt, email } = user
+    //     const query = fastify.db.prepare(`INSERT INTO users (username, password, salt, email) VALUES (?, ?, ?, ?)`)
+    //     const result = query.run(username, password, salt, email)
+    //     fastify.log.debug(`createUser: ${username} -> ID ${result.lastInsertRowid}`)
+    //     return result.lastInsertRowid
+    //   } catch (err) {
+    //     fastify.log.error(`createUser error: ${err.message}`)
+    //     throw new Error('User creation failed')
+    //   }
+    // }
   })
 }, {
-  name: 'userAutoHooks',
-  dependencies: ['database']
+  name: 'userAutoHooks'
 })
---
+++ srcs/database/routes/user/routes.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+
+module.exports.prefixOverride = ''
+module.exports = fp(
+  async function applicationAuth (fastify, opts) {
+    fastify.get('/users/:username', {
+      schema: {
+        params: fastify.getSchema('schema:user:getUser')
+      },
+      response: { 200: fastify.getSchema('schema:user:user')},
+      handler: async function getUser (request, reply) {
+        try {
+          const { username } = request.params;
+          const user = await fastify.dbUsers.getUserByUsername(username)
+          if (!user) {
+            fastify.log.error('User not found')
+            reply.code(404);
+            return { error: 'User not found' }
+          }
+          return user
+        } catch (err) {
+          fastify.log.error(`Error retrieving user: ${err.message}`)
+          reply.code(500)
+          return { error: 'Internal Server Error' }
+        }          
+      }
+    })
+
+  }, {
+    name: 'user',
+    dependencies: [ 'userAutoHooks', 'database']
+})
+
