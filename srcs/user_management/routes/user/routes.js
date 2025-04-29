255c497493979a3b2031181a7a31fd239543a98e
jdagz28 <joshdagoy28@gmail.com>
2025-04-29 12:58:00 +0200

    user routes to implement

--- srcs/user_management/routes/user/routes.js
+++ srcs/user_management/routes/user/routes.js
@@ -5,6 +5,13 @@ const fp = require('fastify-plugin')
 module.exports.prefixOverride = ''
 module.exports = fp(
   async function userRoutes (fastify, opts) {
+    // General user profile
+    // username
+    // email
+    // avatar
+    // nickname
+    // created
+    // change avatar
     fastify.get('/me', {
       onRequest: [fastify.authenticate],
       handler: async (request, reply) => {
@@ -21,19 +28,143 @@ module.exports = fp(
       }
     })
   
+    // User Profile Account Settings / security
+    // email, (change email)
+    // password, (change password)
+    // has_mfa, 
+    // last password change, 
+    // mfa token (or generate)
+    fastify.get('/me/settings', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User Profile
     fastify.get('/:username', {
       onRequest: [fastify.authenticate],
       handler: async (request, reply) => {
-          try {
-            const user = await fastify.usersDataSource.getUserProfile(request.params.username)
-            if (!user) {
-              return reply.status(404).send({ error: 'User not found' })
-            }
-            return reply.status(200).send(user)
-          } catch (err) {
-            fastify.log.error(`Error fetching user: ${err.message}`)
-            return reply.status(500).send({ error: 'Internal Server Error' })
-          }
+      }
+    })
+
+    // User Friends
+    fastify.get('/:username/friends', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // Blocked users
+    fastify.get('/:username/blocked', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User game History
+    fastify.get('/:username/games', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User Stats
+    fastify.get('/:username/stats', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User change password
+    fastify.post('/me/settings/changePassword', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User change email
+    fastify.post('/me/settings/changeEmail', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User change avatar
+    fastify.post('/me/settings/changeAvatar', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User change nickname
+    fastify.post('/me/settings/changeNickname', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User generate MFA
+    fastify.post('/me/settings/generateMFA', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User disable MFA
+    fastify.post('/me/settings/disableMFA', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+    
+    // User enable MFA
+    fastify.post('/me/settings/enableMFA', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User add friend
+    fastify.post('/me/addFriend', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User remove friend
+    fastify.post('/me/removeFriend', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User block user
+    fastify.post('/me/blockUser', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
+      }
+    })
+
+    // User unblock user
+    fastify.post('/me/unblockUser', {
+      onRequest: [fastify.authenticate],
+      handler: async (request, reply) => {
+
       }
     })
   }
