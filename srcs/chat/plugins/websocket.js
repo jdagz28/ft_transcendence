7cda5447986138bb6f9011b8de8f18134d15b2a6
carlosrtzz <or.carlos.pro@gmail.com>
2025-05-16 20:54:26 +0200

    socket with websocket/fastify

---
+++ srcs/chat/plugins/websocket.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+
+module.exports = fp(async function chatPlugin (fastify, opts) {
+
+  await fastify.register(require('@fastify/websocket'))
+
+  fastify.get('/chat', {websocket: true}, (socket, req) => {
+    socket.on('message', message => {
+      socket.send('hi from server')
+    })
+  })
+})
