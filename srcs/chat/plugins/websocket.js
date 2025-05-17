fc577e6b4d376cd438eb66c146fc1797fc9da187
carlosrtzz <or.carlos.pro@gmail.com>
2025-05-17 15:29:52 +0200

    websocket connection done

--- srcs/chat/plugins/websocket.js
+++ srcs/chat/plugins/websocket.js
@@ -3,7 +3,6 @@
 const fp = require('fastify-plugin')
 
 module.exports = fp(async function chatPlugin (fastify, opts) {
-
   await fastify.register(require('@fastify/websocket'))
 
   fastify.get('/chat', {websocket: true}, (socket, req) => {
