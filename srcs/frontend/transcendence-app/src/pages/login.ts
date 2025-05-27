8aedac3a963f2d71c1d854a9d70441a9865e6eea
jdagz28 <joshdagoy28@gmail.com>
2025-05-27 23:42:07 +0200

    login card

--- srcs/frontend/transcendence-app/src/pages/login.ts
+++ srcs/frontend/transcendence-app/src/pages/login.ts
@@ -5,30 +5,38 @@ export function renderLoginPage(): void {
   if (!root) return;
 
   root.innerHTML = /*html*/ `
-    <div class="min-h-screen flex items-center justify-center bg-gradient-to-b from-[#0a1d3b] to-[#0f2a4e]">
-      <div class="bg-[#0d2551] p-8 rounded-xl shadow-lg w-full max-w-sm text-white">
+    <div class="min-h-screen flex items-center justify-center bg-gradient-to-b from-[#0a1d3b] to-[#0f2a4e] selection:bg-blue-400 selection:text-white relative z-10">
+      <div class="bg-[#0d2551] p-8 rounded-xl shadow-lg w-full max-w-md text-white backdrop-blur-sm bg-opacity-90">
         <div class="flex flex-col items-center">
-          <img src="/icons8-rocket.svg" class="w-12 h-12 mb-4" />
-          <h2 class="text-2xl font-bold mb-6">LOGIN</h2>
+          <img src="/icons8-rocket.svg" class="w-22 h-22 mb-6" />
+          <h2 class="text-4xl font-bold mb-8">LOGIN</h2>
         </div>
 
-        <form id="loginForm" class="space-y-4">
+        <form id="loginForm" class="space-y-5">
           <input type="text" id="username" placeholder="Username"
-            class="w-full px-4 py-2 bg-[#081a37] border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-orange-400 placeholder-gray-400" />
+            class="w-full px-4 py-2 bg-[#081a37] border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-sky-400 placeholder-gray-400" />
           <input type="password" id="password" placeholder="Password"
-            class="w-full px-4 py-2 bg-[#081a37] border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-orange-400 placeholder-gray-400" />
+            class="w-full px-4 py-2 bg-[#081a37] border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-sky-400 placeholder-gray-400" />
           <button type="submit"
-            class="w-full bg-gradient-to-r from-orange-500 to-orange-400 text-white font-semibold py-2 rounded-md hover:opacity-90 transition">
+            class="w-full text-xl bg-gradient-to-r from-orange-500 to-orange-400 text-white font-semibold py-3 rounded-md hover:opacity-90 transition">
             Login
           </button>
         </form>
 
-        <div class="flex items-center justify-center my-4 text-gray-400 text-sm">or</div>
-
+        <div class="flex items-center justify-center my-3 text-gray-400 text-md">or</div>
+        <div class="flex gap-3">
+          <button class="google-btn flex-1 bg-white border border-gray-300 text-gray-700 py-3 px-4 rounded-md flex items-center justify-center gap-3 hover:bg-gray-700 hover:shadow-md hover:text-white transition-all shadow-sm">
+            <img src="/icons8-google.svg" class="w-7 h-7" alt="Google logo" />
+            <span class="font-medium text-sm"> Google</span>
+          </button>
+          <button class="intra-btn flex-1 bg-gray-800 text-white py-3 px-4 rounded-md flex items-center justify-center gap-2 hover:bg-gray-700 transition">
+            <span class="font-bold">42</span> Intra
+          </button>
+        </div>
 
         <div id="loginError" class="text-red-400 text-sm mt-3 hidden text-center"></div>
 
-        <div class="text-sm text-center mt-6 text-gray-400">
+        <div class="text-md text-center mt-6 text-gray-400">
           Don’t have an account? <a href="#register" class="text-orange-400 hover:underline">Register</a>
         </div>
       </div>
@@ -42,7 +50,7 @@ export function renderLoginPage(): void {
       const password = (document.getElementById('password') as HTMLInputElement).value;
 
       if (!username || !password) {
-        alert('Username and password are required.');
+        // alert('Username and password are required.');
         return;
       }
 
@@ -70,5 +78,14 @@ export function renderLoginPage(): void {
           errorDiv.classList.remove('hidden');
         }
       }
+    });
+
+
+    document.querySelector('.google-btn')!.addEventListener('click', () => {
+      window.location.href = '/auth/google';
+    });
+
+    document.querySelector('.intra-btn')!.addEventListener('click', () => {
+      window.location.href = '/auth/42';
     });
 }
